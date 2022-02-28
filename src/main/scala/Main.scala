9ecdac9e1f5ac1c8e3d09092b14411d97a85b34b
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-02-28 13:21:22 -0800

    Modifies Main.scala Adds template code from google drive

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,22 @@
 object Main {
+  def main(args: Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
+    println("program started")
+
+    val conf = new SparkConf().setMaster("local[4]").setAppName("kafkar")
+    val ssc = new StreamingContext(conf, Seconds(2))
+
+    //my kafka topic name is 'mytest'
+    val kafkaStream = KafkaUtils.createStream(
+      ssc,
+      "localhost:2181",
+      "spark-streaming-consumer-group",
+      Map("test-topic" -> 5)
+    )
+    kafkaStream.print()
+    ssc.start
+    ssc.awaitTermination()
+  }
 }
