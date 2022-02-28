ad3ec54dda91700839f77c427b1728f7f4cfac38
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-02-28 12:39:17 -0800

    Adds more starting files

---
+++ src/main/scala/Utilities.scala
@@ -0,0 +1,103 @@
+import org.apache.spark.sql.DataFrame
+import java.io.{File, FileOutputStream, PrintWriter}
+import scala.io.Source
+import scala.io.StdIn.readLine
+
+object Utilities {
+  def chooseN(n: Byte): Byte = {
+    var input: Char = readLine().trim().charAt(0)
+    var inByte: Byte = 0
+    var goodIn: Boolean = false
+
+    n match {
+      case 1 =>
+        println(
+          "Sorry, but you have to choose '1'... Huh... almost feels like you have no choice at all... OK you can go now. "
+        );
+        goodIn = true;
+        inByte = 1.toByte
+      case 2 =>
+        while (!goodIn) {
+          input match {
+            case '1' => goodIn = true; inByte = 1.toByte
+            case '2' => goodIn = true; inByte = 2.toByte
+            case _ =>
+              println("Sorry, but you have to choose '1', or '2': ");
+              input = readLine().trim().charAt(0)
+          }
+        }
+      case 3 =>
+        while (!goodIn) {
+          input match {
+            case '1' => goodIn = true; inByte = 1.toByte
+            case '2' => goodIn = true; inByte = 2.toByte
+            case '3' => goodIn = true; inByte = 3.toByte
+            case _ =>
+              println("Sorry, but you have to choose '1', '2', or '3': ");
+              input = readLine().trim().charAt(0)
+          }
+        }
+      case 4 =>
+        while (!goodIn) {
+          input match {
+            case '1' => goodIn = true; inByte = 1.toByte
+            case '2' => goodIn = true; inByte = 2.toByte
+            case '3' => goodIn = true; inByte = 3.toByte
+            case '4' => goodIn = true; inByte = 4.toByte
+            case _ =>
+              println("Sorry, but you have to choose '1', '2', '3', or '4': ");
+              input = readLine().trim().charAt(0)
+          }
+        }
+      case 5 =>
+        while (!goodIn) {
+          input match {
+            case '1' => goodIn = true; inByte = 1.toByte
+            case '2' => goodIn = true; inByte = 2.toByte
+            case '3' => goodIn = true; inByte = 3.toByte
+            case '4' => goodIn = true; inByte = 4.toByte
+            case '5' => goodIn = true; inByte = 5.toByte
+            case _ =>
+              println(
+                "Sorry, but you have to choose '1', '2', '3', '4', or '5': "
+              )
+              input = readLine().trim().charAt(0)
+          }
+        }
+      case 6 =>
+        while (!goodIn) {
+          input match {
+            case '1' => goodIn = true; inByte = 1.toByte
+            case '2' => goodIn = true; inByte = 2.toByte
+            case '3' => goodIn = true; inByte = 3.toByte
+            case '4' => goodIn = true; inByte = 4.toByte
+            case '5' => goodIn = true; inByte = 5.toByte
+            case '6' => goodIn = true; inByte = 6.toByte
+            case _ =>
+              println(
+                "Sorry, but you have to choose '1', '2', '3', '4', '5', or '6': "
+              )
+              input = readLine().trim().charAt(0)
+          }
+        }
+      case 7 =>
+        while (!goodIn) {
+          input match {
+            case '1' => goodIn = true; inByte = 1.toByte
+            case '2' => goodIn = true; inByte = 2.toByte
+            case '3' => goodIn = true; inByte = 3.toByte
+            case '4' => goodIn = true; inByte = 4.toByte
+            case '5' => goodIn = true; inByte = 5.toByte
+            case '6' => goodIn = true; inByte = 6.toByte
+            case '7' => goodIn = true; inByte = 7.toByte
+            case _ =>
+              println(
+                "Sorry, but you have to choose '1', '2', '3', '4', '5', '6', or '7': "
+              )
+              input = readLine().trim().charAt(0)
+          }
+        }
+    }
+    inByte
+  }
+}
