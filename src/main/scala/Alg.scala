e94d64220d8cfeae9d58d85f3d7fef654ceb6a98
SamuelTaylr <89416553+SamuelTaylr@users.noreply.github.com>
2022-03-01 12:10:23 -0800

    Added timestamp generator

--- src/main/scala/Alg.scala
+++ src/main/scala/Alg.scala
@@ -1,11 +1,24 @@
 import scala.util.Random._
 import scala.math.BigDecimal._
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
-def priceGen(): Double = {
-  //creates random Int
-  val whole = nextInt(5000)
-  //creates random Float
-  val dec = nextFloat()
-  val price = whole + dec.setScale(2, BigDecimal.RoundingMode.HALF_UP).toFloat
-  return price
+object Alg {
+  def yourFunctions(): Unit = {}
+
+  def timestampGenerator: String = {
+    val now = Calendar.getInstance().getTime()
+    val newDate = now.toString
+
+    return newDate
+  }
+
+  def priceGen(): Double = {
+    //creates random Int
+    val whole = nextInt(5000)
+    //creates random Float
+    val dec = nextFloat()
+    val price = whole + dec.setScale(2, BigDecimal.RoundingMode.HALF_UP).toFloat
+    return price
+
 }
