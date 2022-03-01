47e8df71933294c8aafe07351b4e52ea959f6b29
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-03-01 09:51:35 -0800

    Fixes build.sbt and Main.scala imports

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,50 @@
+<<<<<<< Updated upstream
+||||||| constructed merge base
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka
+import org.apache.spark.streaming.StreamingContext._
+
+=======
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka
+import org.apache.spark.streaming.StreamingContext._
+
+>>>>>>> Stashed changes
 object Main {
+<<<<<<< Updated upstream
+||||||| constructed merge base
+  val spark = SparkSession.builder
+    .master("local[*]")
+    .appName("Spark Word Count")
+    .enableHiveSupport()
+    .getOrCreate()
+
+  def main(args: Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    println("program started")
+
+    val conf = new SparkConf().setMaster("local[4]").setAppName("kafkar")
+    val ssc = new StreamingContext(conf, Seconds(2))
+=======
+  def main(args: Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    println("program started")
+
+    val conf = new SparkConf().setMaster("local[4]").setAppName("kafkar")
+    val ssc = new StreamingContext(conf, Seconds(5))
+>>>>>>> Stashed changes
 
 }
