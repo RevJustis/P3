d2ece5f626592ad33bdc5ef798c30a2f308cd94c
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-03-02 13:13:36 -0800

    Commented out unused code marked with link of origin

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,25 +9,26 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 object Main {
 
   def main(args: Array[String]): Unit = {
-    Logger.getLogger("org").setLevel(Level.OFF)
-    Logger.getLogger("akka").setLevel(Level.OFF)
-
-    println("program started")
-
-    val conf = new SparkConf().setMaster("local[4]").setAppName("kafkar")
-    val ssc = new StreamingContext(conf, Seconds(2))
-
-    // my kafka topic name is 'mytest'
-    val kafkaStream = KafkaUtils.createStream(
-      ssc,
-      "localhost:2181",
-      "spark-streaming-consumer-group",
-      Map("test-topic" -> 5)
-    )
-    kafkaStream.print()
-    ssc.start
-    ssc.awaitTermination()
     hostGen()
 
+    // This is the code from https://drive.google.com/drive/u/1/folders/1t8XbhV7p99POeHhWi6PAJBZ8p1KyDZMJ
+    // Logger.getLogger("org").setLevel(Level.OFF)
+    // Logger.getLogger("akka").setLevel(Level.OFF)
+
+    // println("program started")
+
+    // val conf = new SparkConf().setMaster("local[4]").setAppName("kafkar")
+    // val ssc = new StreamingContext(conf, Seconds(2))
+
+    // // my kafka topic name is 'mytest'
+    // val kafkaStream = KafkaUtils.createStream(
+    //   ssc,
+    //   "localhost:2181",
+    //   "spark-streaming-consumer-group",
+    //   Map("test-topic" -> 5)
+    // )
+    // kafkaStream.print()
+    // ssc.start
+    // ssc.awaitTermination()
   }
 }
