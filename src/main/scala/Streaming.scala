b011a10800457f7772646b9c89df0aacb675a9f0
revaleo <leo.chen@revature.net>
2022-03-04 11:43:32 -0800

    updated Streaming.scala and Producer.scala

--- src/main/scala/Streaming.scala
+++ src/main/scala/Streaming.scala
@@ -1,9 +1,9 @@
-import java.util
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
+import java.security.Key
 import scala.collection.JavaConverters._
 
 object streaming {
@@ -17,28 +17,43 @@ object streaming {
       .config("spark.master","local")
       .getOrCreate()
 
+    import spark.implicits._
+
+    val mySchema = StructType(Array(
+      StructField("order_id", StringType),
+      StructField("customer_id", StringType),
+      StructField("customer_name", StringType),
+      StructField("product_id", StringType),
+      StructField("product_name", StringType),
+      StructField("product_category", StringType),
+      StructField("payment_type", StringType),
+      StructField("qty", StringType),
+      StructField("price", StringType),
+      StructField("datetime", StringType),
+      StructField("country", StringType),
+      StructField("city", StringType),
+      StructField("ecommerce_website_name", StringType),
+      StructField("payment_txn_id", StringType),
+      StructField("payment_txn_success", StringType),
+      StructField("failure_reason", StringType)
+    ))
 
     val df = spark.readStream.format("kafka")
       .option("kafka.bootstrap.servers", "[::1]:9092")
-      .option("subscribe", "Topic")
+      .option("subscribe", "ingest")
+      .option("startingOffsets", "earliest")
       .load()
-      .select(col("value").cast("string"))
+      .select(col("value").cast("String"))
+      .select(from_json(col("value"),mySchema).as("table"))
+      .select("table.*")
+
 
     df.printSchema()
-    import spark.implicits._
-    //Doesnt work as intended, method on line 25 replaces line 30
-    //df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)","CAST(topic AS STRING)","CAST(partition AS STRING)","CAST(offset AS STRING)","CAST(timestamp AS STRING)","CAST(timestampType AS STRING)").as[(String,String,String,String,String,String,String)]
-    val query = df.writeStream
+    df.writeStream
       .outputMode("append")
       .format("console")
       .start()
-
-    // some statement
-    // some statement
-    query.awaitTermination();
-
-
-
+      .awaitTermination()
   }
 }
 
