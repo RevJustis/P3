85771a13aa7987c59bea01f1b8ec0a2fdec4134a
revaleo <leo.chen@revature.net>
2022-03-06 19:51:53 -0800

    Updated Streaming.scala and Producer.scala AGAIN --Now Both CSV and Json are available for producer and consumer(streaming.scala) --Please comment/uncomment the marked section to test both functionality --Added config to make ProgressReporter timer huge(no progress report bewteen)

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -9,28 +9,27 @@ object Producer {
 
       val key = getKey()
 
-      val order_id = "23"
-      val customer_id = "1011"
-      val customer_name = "leo chen"
-      val product_id = "1234"
-      val product_name = "xbox"
-      val product_category = "gaming"
-      val payment_type = "visa"
-      val qty = "20"
-      val price = "499"
-      val datetime = "2022-03-04 10:00"
-      val country = "USA"
-      val city = "Los Angeles"
-      val ecommerce_website_name = "www.microsoft.com"
-      val payment_txn_id = "56789"
-      val payment_txn_success = "Y"
-      val failure_reason = ""
+      val order_id="23"
+      val customer_id="1011"
+      val customer_name= "leo chen"
+      val product_id="1234"
+      val product_name="xbox"
+      val product_category="gaming"
+      val payment_type="visa"
+      val qty="20"
+      val price="499"
+      val datetime="2022-03-04 10:00"
+      val country="USA"
+      val city="Los Angeles"
+      val ecommerce_website_name="www.microsoft.com"
+      val payment_txn_id="56789"
+      val payment_txn_success="Y"
+      val failure_reason=""
 
-      //hard code adding record:
-      //val value = """{"order_id": "1","customer_id": "101","customer_name": "John Smith","product_id": "201","product_name": "Pen","product_category": "Stationery","payment_type": "Card","qty": "24","price": "10","datetime": "2021-01-10 10:12","country": "India","city": "Mumbai","ecommerce_website_name": "www.amazon.com","payment_txn_id": "36766","payment_txn_success": "Y","failure_reason": ""}"""
-      val value = s"""{"order_id": "$order_id","customer_id": "$customer_id","customer_name": "$customer_name","product_id": "$product_id","product_name": "$product_name","product_category": "$product_category","payment_type": "$payment_type","qty": "$qty","price": "$price","datetime": "$datetime","country": "$country","city": "$city","ecommerce_website_name": "$ecommerce_website_name","payment_txn_id": "$payment_txn_id","payment_txn_success": "$payment_txn_success","failure_reason": "$failure_reason"}"""
-
-
+      //send json
+      //  val value = s"""{"order_id": "$order_id","customer_id": "$customer_id","customer_name": "$customer_name","product_id": "$product_id","product_name": "$product_name","product_category": "$product_category","payment_type": "$payment_type","qty": "$qty","price": "$price","datetime": "$datetime","country": "$country","city": "$city","ecommerce_website_name": "$ecommerce_website_name","payment_txn_id": "$payment_txn_id","payment_txn_success": "$payment_txn_success","failure_reason": "$failure_reason"}"""
+      //send csv
+      val value = s"$order_id,"+s"$customer_id,"+s"$customer_name,"+s"$product_id,"+s"$product_name,"+s"$product_category,"+s"$payment_type,"+s"$qty,"+s"$price,"+s"$datetime,"+s"$country,"+s"$city,"+s"$ecommerce_website_name,"+s"$payment_txn_id,"+s"$payment_txn_success,"+s"$failure_reason,"
       val record = new ProducerRecord[String, String](
         topic,
         key,
@@ -47,29 +46,35 @@ object Producer {
     }
 
     val props = new Properties()
+    //3.86.155.113:9092
     props.put("bootstrap.servers", "[::1]:9092")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
     val producer = new KafkaProducer[String, String](props)
 
-    while (true) {
-      try {
-        //testing, sending a single record
-        sendRecord(producer, "ingest")
-        Thread.sleep(2000)
+    try {
+      //    while (true) {
 
+      //send csv
+      sendRecord(producer, "csv")
+      //send json
+      //  sendRecord(producer,"json")
 
-      } catch {
-        case e: Exception => {
-          e.printStackTrace()
-        }
+      Thread.sleep(2000)
+      //
+
+      //    }
+
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
       }
-      /*finally {
-        producer.close()
-        }
-       */
-
+    } finally {
+      producer.close()
     }
+
+
+
   }
 }
