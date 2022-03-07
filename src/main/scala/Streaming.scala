f89121c3ad38d87e3825257df54b3e60147ef465
revaleo <leo.chen@revature.net>
2022-03-06 19:51:53 -0800

    Updated Streaming.scala and Producer.scala AGAIN --Now Both CSV and Json are available for producer and consumer(streaming.scala) --Please comment/uncomment the marked section to test both functionality --Added config to make ProgressReporter timer huge(no progress report bewteen)

--- src/main/scala/Streaming.scala
+++ src/main/scala/Streaming.scala
@@ -1,6 +1,8 @@
+import java.util
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
-import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.functions.{col, from_json,split}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 import java.security.Key
@@ -15,39 +17,66 @@ object streaming {
       .builder()
       .appName("test")
       .config("spark.master","local")
+      .config("spark.sql.streaming.noDataProgressEventInterval", 999999999)
       .getOrCreate()
 
-    import spark.implicits._
 
-    val mySchema = StructType(Array(
-      StructField("order_id", StringType),
-      StructField("customer_id", StringType),
-      StructField("customer_name", StringType),
-      StructField("product_id", StringType),
-      StructField("product_name", StringType),
-      StructField("product_category", StringType),
-      StructField("payment_type", StringType),
-      StructField("qty", StringType),
-      StructField("price", StringType),
-      StructField("datetime", StringType),
-      StructField("country", StringType),
-      StructField("city", StringType),
-      StructField("ecommerce_website_name", StringType),
-      StructField("payment_txn_id", StringType),
-      StructField("payment_txn_success", StringType),
-      StructField("failure_reason", StringType)
-    ))
+    //json
+    //    val mySchema = StructType(Array(
+    //      StructField("order_id", StringType),
+    //      StructField("customer_id", StringType),
+    //      StructField("customer_name", StringType),
+    //      StructField("product_id", StringType),
+    //      StructField("product_name", StringType),
+    //      StructField("product_category", StringType),
+    //      StructField("payment_type", StringType),
+    //      StructField("qty", StringType),
+    //      StructField("price", StringType),
+    //      StructField("datetime", StringType),
+    //      StructField("country", StringType),
+    //      StructField("city", StringType),
+    //      StructField("ecommerce_website_name", StringType),
+    //      StructField("payment_txn_id", StringType),
+    //      StructField("payment_txn_success", StringType),
+    //      StructField("failure_reason", StringType)
+    //    ))
+    //    val df = spark.readStream.format("kafka")
+    //      //3.86.155.113:9092
+    //      .option("kafka.bootstrap.servers", "[::1]:9092")
+    //      .option("startingOffsets", "earliest")
+    //      .option("subscribe", "json")
+    //      .load()
+    //      .select(col("value").cast("String"))
+    //      .select(from_json(col("value"),mySchema).as("table"))
+    //      .select("table.*")
+
+
+    //csv
 
     val df = spark.readStream.format("kafka")
+      //3.86.155.113:9092
       .option("kafka.bootstrap.servers", "[::1]:9092")
-      .option("subscribe", "ingest")
       .option("startingOffsets", "earliest")
+      .option("subscribe", "csv")
       .load()
-      .select(col("value").cast("String"))
-      .select(from_json(col("value"),mySchema).as("table"))
-      .select("table.*")
+      .select(split(col("value"),",").getItem(0).as("order_id"),
+        split(col("value"),",").getItem(1).as("customer_id"),
+        split(col("value"),",").getItem(2).as("customer_name"),
+        split(col("value"),",").getItem(3).as("product_id"),
+        split(col("value"),",").getItem(4).as("product_name"),
+        split(col("value"),",").getItem(5).as("product_category"),
+        split(col("value"),",").getItem(6).as("payment_type"),
+        split(col("value"),",").getItem(7).as("qty"),
+        split(col("value"),",").getItem(8).as("price"),
+        split(col("value"),",").getItem(9).as("datetime"),
+        split(col("value"),",").getItem(10).as("country"),
+        split(col("value"),",").getItem(11).as("city"),
+        split(col("value"),",").getItem(12).as("ecommerce_website_name"),
+        split(col("value"),",").getItem(13).as("payment_txn_id"),
+        split(col("value"),",").getItem(14).as("payment_txn_success"),
+        split(col("value"),",").getItem(15).as("failure_reason"))
 
-
+    //Both need the following
     df.printSchema()
     df.writeStream
       .outputMode("append")
