78b27f85b58178cc9f6a08e4ca0a104894c3b59b
RevJustis <97845336+RevJustis@users.noreply.github.com>
2022-03-07 21:00:11 -0800

    Modifies txt files and scala objects Adds some debugging prints

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,7 +18,6 @@ object Main {
     .master("local[*]")
     .appName("P3")
     .getOrCreate()
-  spark.sparkContext.setLogLevel("ERROR")
   // Create the DataFrames at global scope so that they are made once and used many times
   val dfA = spark.read
     .parquet("input/pq/amazon.parquet")
@@ -41,74 +40,18 @@ object Main {
     .withColumn("Price", col("Price").cast(DoubleType))
     .select("Title", "Price", "Pageurl")
 
+  var ID = 1 // starting point for the incrementing id
+
   def main(args: Array[String]): Unit = {
+    spark.sparkContext.setLogLevel("ERROR")
     dfA.persist(StorageLevel.MEMORY_ONLY_SER_2)
     dfW.persist(StorageLevel.MEMORY_ONLY_SER_2)
     dfE.persist(StorageLevel.MEMORY_ONLY_SER_2)
 
-    var id = 1 // starting point for the incrementing id
     var i = 0
-    while (i < 10) {
-      val t1 = System.nanoTime
-      var record = mutable.Map[String, String]()
-      // Order ID and timestamp generation
-      record += ("order_id" -> id.toString, "datetime" -> timestampGen)
-      id = id + 1
-
-      // Price, unit price and quantity gen
-      val price = priceGen // Total, Unit, qty
-      record += (
-        "price" -> price._1.toString(),
-        "unitPrice" -> price._2.toString(),
-        "qty" -> price._3.toString()
-      )
-
-      // Payment info generation
-      val pay = payStatusGen
-      record += (
-        "payment_type" -> payTypeGen,
-        "payment_txn_id" -> payIdGen,
-        "payment_txn_success" -> pay._1,
-        "failure_reason" -> pay._2
-      )
-
-      // Customer info gen
-      val customer = cusRecord(nextInt(1000), isEnemyName(pay._1))
-      record += (
-        "customer_id" -> customer._1,
-        "customer_name" -> customer._2
-      )
-
-      // create url and store
-      val host = hostNameGen
-      // record += ("ecommerce_website_name" -> urlGen(host, customer._2))
-      // Product info generation
-      val product = proRecord(nextInt(1000), price._2, host, spark)
-      record += (
-        "product_id" -> product._1,
-        "product_name" -> product._2,
-        "product_category" -> product._3,
-        "original_price" -> product._4,
-        "ecommerce_website_name" -> product._5
-      )
-
-      val location = cityCountryGen(spark)
-      record += (
-        "city" -> location._1,
-        "country" -> location._2
-      )
-
-      println(
-        record
-      ) // Print the record (the map) to console so we can see that it is all good
-
-      val duration = (System.nanoTime - t1) / 1e9d
-
-      println(
-        "The execution time of the function is: " + duration + " seconds."
-      )
-      println("*******************")
-
+    while (i < 100) {
+      println(getMap())
+      println("*************************")
       i += 1
     }
   }
